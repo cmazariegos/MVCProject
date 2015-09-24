a5402c26b2bd92ee65984b561e9a98b3f880f80c
cmazariegos <the.master.of.the.systems64gmail.com>
2015-09-24 15:41:48 -0600

    correo html

--- libs/sendMail.php
+++ libs/sendMail.php
@@ -3,6 +3,29 @@ class sendMail {
     
     private static $sInstance = null;
     
+    public function createHTML($init,$body,$end){
+        $cuerpoTabla='';
+        foreach ($body as $key => $val) {
+            $cuerpoTabla=$cuerpoTabla.'<tr>
+                                           <td><label>'.$key.'</label></td><td><input type="text" value="'.$val.'" readonly></td>
+                                       </tr>';            
+        }
+        $texto = '
+                <html>
+                <head>
+                  <title>Contact</title>
+                </head>
+                <body>
+                  '.$init.'
+                  <table>
+                       '.$cuerpoTabla.'
+                  </table>
+                  '.$end.'                  
+                </body>
+                </html>
+                ';
+        return $texto;
+    }
     public function send($subject,$txt,$from){
         $header = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n".'From: '.$from. "\r\n";
         mail('info@e-ssisa.com', '=?UTF-8?B?'.base64_encode($subject).'?=', $txt, $header);
