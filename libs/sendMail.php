e37cba51c7f0f9e9a7b6547b4ff299124b6a701e
cmazariegos <the.master.of.the.systems@gmail.com>
2015-09-24 17:30:26 -0600

    working html mails

--- libs/sendMail.php
+++ libs/sendMail.php
@@ -27,7 +27,7 @@ class sendMail {
         return $texto;
     }
     public function send($subject,$txt,$from){
-        $header = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/plain; charset=UTF-8' . "\r\n".'From: '.$from. "\r\n";
+        $header = 'MIME-Version: 1.0' . "\r\n" . 'Content-type: text/html; charset=UTF-8' . "\r\n".'From: '.$from. "\r\n";
         mail('info@e-ssisa.com', '=?UTF-8?B?'.base64_encode($subject).'?=', $txt, $header);
         mail("cvirtual2003@hotmail.com",'=?UTF-8?B?'.base64_encode($subject).'?=',$txt);  
         mail("carlos.mazariegos@e-ssisa.com",$subject,$txt,$from);        
